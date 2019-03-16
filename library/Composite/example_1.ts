0c2d84dc5ee4e447b58c9ea319b82707ed3c6bc9
Khaled Mohamed <khal3d.mohamed@gmail.com>
2019-03-16 22:56:43 +0200

    Added Composite Pattern README, Example, and Tests

---
+++ library/Composite/example_1.ts
@@ -0,0 +1,59 @@
+export interface GUIElement {
+  draw(): string;
+}
+
+abstract class CompositeGUIElement implements GUIElement {
+  private _children: GUIElement[];
+
+  public constructor() {
+    this._children = [];
+  }
+
+  addElement(element: GUIElement): void {
+    this._children.push(element);
+  }
+
+  removeElement(element: GUIElement): void {
+    this._children = this._children.filter(e => e !== element);
+  }
+
+  getChild(index: number): GUIElement {
+    return this._children[index];
+  }
+
+  draw(): string {
+    let children = this._children.map(child => child.draw());
+
+    return children.join(', ');
+  }
+}
+
+export class Window extends CompositeGUIElement {
+  draw(): string {
+    return 'Window(' + super.draw() + ')';
+  }
+}
+
+export class Panel extends CompositeGUIElement {
+  draw(): string {
+    return 'Panel(' + super.draw() + ')';
+  }
+}
+
+export class Button implements GUIElement {
+  draw(): string {
+    return 'Button';
+  }
+}
+
+export class TextBox implements GUIElement {
+  draw(): string {
+    return 'TextBox';
+  }
+}
+
+export class InputForm implements GUIElement {
+  draw(): string {
+    return 'InputForm';
+  }
+}
---
+++ library/Composite/index.test.ts
@@ -0,0 +1,51 @@
+import { Button, Panel, TextBox, InputForm, Window } from './example_1';
+
+describe('Composite Pattern', () => {
+  describe('Example 1 Tests', () => {
+    const button = new Button();
+    const button2 = new Button();
+    const button3 = new Button();
+    const textBox = new TextBox();
+
+    const buttonsPanel = new Panel();
+    buttonsPanel.addElement(button);
+    buttonsPanel.addElement(button2);
+    buttonsPanel.addElement(button3);
+    buttonsPanel.addElement(textBox);
+
+    const bigPanel = new Panel();
+    const inputForm = new InputForm();
+    const textBox2 = new TextBox();
+    bigPanel.addElement(textBox2);
+    bigPanel.addElement(inputForm);
+    bigPanel.addElement(buttonsPanel);
+
+    const window = new Window();
+    const textBox3 = new TextBox();
+    window.addElement(textBox3);
+    window.addElement(bigPanel);
+
+    it('should render the correct tree', () => {
+      const expectedTree =
+        'Window(TextBox, Panel(TextBox, InputForm, Panel(Button, Button, Button, TextBox)))';
+
+      expect(window.draw()).toBe(expectedTree);
+    });
+
+    it('should remove item correclty', () => {
+      window.removeElement(bigPanel);
+      const expectedTree = 'Window(TextBox)';
+
+      expect(window.draw()).toBe(expectedTree);
+      window.addElement(bigPanel);
+    });
+
+    it('should add item correclty', () => {
+      window.addElement(new InputForm());
+      window.removeElement(bigPanel);
+      const expectedTree = 'Window(TextBox, InputForm)';
+
+      expect(window.draw()).toBe(expectedTree);
+    });
+  });
+});
